8540225ccbab27ef97ec62fc8e3d7b3d24b3a539
MechCoder <manojkumarsivaraj334@gmail.com>
2017-05-30 06:15:51 +0530

    Update README

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='mondrian-art',
-      version='0.1',
+      version='0.1.1',
       description='Generate modern art using mondrian processes.',
       license='BSD',
-      author='Manoj Kumar',
+      author=['Manoj Kumar', 'Lilian Besson'],
       packages=['mondrian_art'],
       install_requires=["numpy", "matplotlib"]
 )
