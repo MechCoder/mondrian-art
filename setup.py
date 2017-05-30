a112b4fb5fd97df82275af03af438d8c81b97d68
MechCoder <manojkumarsivaraj334@gmail.com>
2017-05-30 19:57:38 +0530

    Add comma

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='mondrian-art',
-      version='0.1.1',
+      version='0.1.2',
       description='Generate modern art using mondrian processes.',
       license='BSD',
       maintainer='Manoj Kumar',
-      maintainer_email="mks542@nyu.edu"
+      maintainer_email="mks542@nyu.edu",
       packages=['mondrian_art'],
       install_requires=["numpy", "matplotlib"]
 )
